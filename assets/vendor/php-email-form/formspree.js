58e624d9ec2ef9ca852363b2d995f2860ddb16fe
RikHart <rsrodriguez06@gmail.com>
2022-03-19 17:55:02 -0600

    corrección status

--- assets/vendor/php-email-form/formspree.js
+++ assets/vendor/php-email-form/formspree.js
@@ -4,6 +4,8 @@ async function handleSubmit(event) {
   event.preventDefault();
   var status = document.getElementById("status");
   var data = new FormData(event.target);
+  status.classList.add("loading");
+  status.classList.add("d-block");
   fetch(event.target.action, {
     method: form.method,
     body: data,
@@ -13,8 +15,8 @@ async function handleSubmit(event) {
   })
     .then((response) => {
       if (response.ok) {
+        status.classList.remove("loading");
         status.classList.add("sent-message");
-        status.classList.add("d-block");
         status.innerHTML = "Tu mensaje ha sido enviado. ¡Gracias!";
         form.reset();
       } else {
@@ -24,6 +26,7 @@ async function handleSubmit(event) {
               .map((error) => error["message"])
               .join(", ");
           } else {
+            status.classList.remove("loading");
             status.classList.add("error-message");
             status.innerHTML =
               "¡Oops! Algo salió mal. Por favor, utiliza otro medio. Gracias.";
@@ -32,8 +35,8 @@ async function handleSubmit(event) {
       }
     })
     .catch((error) => {
+      status.classList.remove("loading");
       status.classList.add("error-message");
-      status.classList.add("d-block");
       status.innerHTML =
         "¡Oops! Algo salió mal. Por favor, utiliza otro medio. Gracias.";
     });
